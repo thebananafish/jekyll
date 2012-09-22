fa82c9fec436042304fea0400bc23a1097edf47c
Eugene Yum <eugene.yum@gmail.com>
2012-09-22 11:43:49 -0400

    add pid file to unicorn to stop it easier

--- unicorn.rb
+++ unicorn.rb
@@ -1,3 +1,11 @@
-worker_processes 4 # amount of unicorn workers to spin up
-timeout 30         # restarts workers that hang for 30 seconds
-preload_app true   # avoid regeneration of jekyll site for each fork
+# Create a pidfile so we can kill the server through our action hooks.
+pid File.join(ENV["OPENSHIFT_REPO_DIR"], "tmp/pids/unicorn.pid")
+
+# amount of unicorn workers to spin up
+worker_processes 4
+
+# restarts workers that hang for 30 seconds
+timeout 30
+
+# avoid regeneration of jekyll site for each fork
+preload_app true
